d3b3348acb63d500981199253f725cb4dff2e736
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-09-11 08:46:03 -0600

    local properties

--- app/src/main/java/com/diegocal/laboratorio6/RetrofitPexels.kt
+++ app/src/main/java/com/diegocal/laboratorio6/RetrofitPexels.kt
@@ -1,5 +1,6 @@
 package com.diegocal.laboratorio6
 
+
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
@@ -16,10 +17,29 @@ object RetrofitPexels {
         level = HttpLoggingInterceptor.Level.BODY
     }
 
-    // Cliente OkHttp para peticiones
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor { chain ->
             val originalRequest = chain.request()
-            //
+
+            val newRequest = originalRequest.newBuilder()
+                .header("Authorization", BuildConfig.PEXELS_API_KEY)
+                .build()
+            chain.proceed(newRequest)
         }
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .build()
+
+    val retrofitService: PexelsApiService by lazy {
+        retrofit.create(PexelsApiService::class.java)
+    }
 }
